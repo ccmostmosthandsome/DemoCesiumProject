b034b3fcded90c8bd1eaddaa5f1a6bd06546d1b6
lujiancheng <1758973467@qq.com>
2018-03-23 09:01:22 +0800

    finish test 剖面分析， update readme.md

---
+++ js/CesiumUtil.js
@@ -0,0 +1,24 @@
+/**
+ * 将世界坐标系批量转为WebMercator坐标系
+ * @param {Array} positions 
+ * @param {Cesium.Globe} globe 
+ */
+function WorldCoord2WebMercatorCoord(positions,globe){
+    if(Array.isArray(positions)){
+        throw new Error("position is not array");
+    }
+    if(globe===undefined){
+        throw new Error("you must input globe param");
+    }
+    let len=positions.len;
+    var arr=[];
+    const projection=new Cesium.WebMercatorProjection();
+    const wgs84globe=globe;
+    for(let cartesian of positions){
+        let cartographic=Cesium.Cartographic.fromCartesian(cartesian);
+        let pro=projection.project(cartographic);
+        pro.height=wgs84globe.getHeight(cartographic);
+        arr.push(pro);
+    }
+    return arr;
+}
